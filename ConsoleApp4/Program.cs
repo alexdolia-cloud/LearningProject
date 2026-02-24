4bd6481e867c0a6a3be00b9f123d4dbb714f0934
Jeffrey Willis <dzabrodskiy03@gmail.com>
2026-02-24 18:25:55 +0200

    useless code

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,9 +1,18 @@
 ﻿using ConsoleApp4.Armours;
+using ConsoleApp4.Armours.ConcreteArmours;
 
 namespace ConsoleApp4
 {
     internal class Program
     {
+        // Useless code designed by junior dev\
+
+
+
+
+
+        // dsadas
+        //d dasda dsa 
         static void Main(string[] args)
         {
             SnakeArmour snakeArmour = new SnakeArmour();
