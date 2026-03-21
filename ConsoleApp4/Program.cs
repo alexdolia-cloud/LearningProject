d72068cd7c5817ad4175210f3ad4a7dc0bc28d85
Jeffrey Willis <dzabrodskiy03@gmail.com>
2026-03-21 18:15:47 +0200

    Fix formatting at Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace ConsoleApp4
 {
     public delegate void Math(int result);
+
     internal class Program
     {
         static void Main(string[] args)
@@ -8,14 +9,17 @@
             Multiply(5, 7, Print);
             Sum(5, 7, Print);
         }
+
         public static void Multiply(int a, int b, Math math)
         {
             math.Invoke(a * b);
         }
+
         public static void Sum(int a, int b, Math math)
         {
             math.Invoke(a + b);
         }
+
         public static void Print(int result)
         {
             Console.WriteLine(result);
