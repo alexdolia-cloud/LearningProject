2026a9febb18825adfa62b9ea8314c1b76a149ca
alex_ <alex_dolia@i.ua>
2026-03-22 18:02:56 +0200

    Add FormChanger class

---
+++ ConsoleApp4/FormChanger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public delegate void CharacterForm(string form);
+    internal class FormChanger
+    {
+        public event CharacterForm Cat;
+        public event CharacterForm Dog;
+
+        public string Form { get; set; }
+
+        public void CatForm(string catForm)
+        {
+            Form = catForm;
+        }
+
+        public void DogForm(string dogForm)
+        {
+            Form = dogForm;
+        }
+    }
+}
