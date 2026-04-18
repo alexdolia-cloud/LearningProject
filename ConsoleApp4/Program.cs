907d4e3e13fe307eb559f696e30f9b17bc6a8095
alex_ <alex_dolia@i.ua>
2026-04-18 18:58:48 +0300

    Mark notes fixed

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,7 +8,7 @@
 
             Console.WriteLine("Welcome");
 
-            todolist.PrintText();
+            PrintText();
 
             while (true)
             {
@@ -16,33 +16,86 @@
 
                 if (userInput == "1")
                 {
-                    todolist.AddNotes();
+                    Console.WriteLine("Please add your note");
+                    string userTask = Console.ReadLine();
+
+                    todolist.AddNotes(userTask);
+
+                    Console.WriteLine("Your note has been added");
+                    PrintText();
                 }
                 else if (userInput == "2")
                 {
-                    todolist.RemoveNotes();
+                    Console.WriteLine("Please write number of note which you want to erase");
+                    string removeItem = Console.ReadLine();
+                    todolist.RemoveNotes(removeItem);
+                    if (todolist.Tasks.Any())
+                    {
+                        Console.WriteLine("There is no notes to remove");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your note was removed");
+                    }
                 }
                 else if (userInput == "3")
                 {
-                    todolist.ShowNotes();
+                    if (!todolist.Tasks.Any())
+                    {
+                        Console.WriteLine("You haven't add any notes yet");
+                    }
+                    else
+                    {
+                        string mark = "x";
+                        todolist.ShowNotes(mark);
+                    }
                 }
                 else if (userInput == "4")
                 {
-                    todolist.MarkNotes();
+                    bool check = false;
+                    string markInput = Console.ReadLine();
+                    todolist.MarkNotes(markInput, check);
+                    if (check ==  false)
+                    {
+                        Console.WriteLine("Your note has been marked");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your note has been unmarked");
+                    }
                 }
                 else if (userInput == "5")
                 {
-                    todolist.UnMarkNotes();
+                    Tutorial();
                 }
                 else if (userInput == "6")
                 {
-                    todolist.Tutorial();
+                    Console.WriteLine("Program is end");
+                    break;
                 }
                 else
                 {
                     Console.WriteLine("Wrong input please try again");
                 }
             }
+        }
+        public static void PrintText()
+        {
+            Console.WriteLine("Input 1 to add new task");
+            Console.WriteLine("Input 2 to remove task");
+            Console.WriteLine("Input 3 to see your tasks");
+            Console.WriteLine("Input 4 to mark tasks");
+            Console.WriteLine("Input 5 for tutorial");
+            Console.WriteLine("Input 6 to end program");
+        }
+
+        public static void Tutorial()
+        {
+            Console.WriteLine("If you input 1 you have to write task which will appear when you using (see tasks) ");
+            Console.WriteLine("If you input 2 you could remove your task (you have to input 3 to see your task's number)");
+            Console.WriteLine("If you input 3 you could see all your tasks and status. You can change status if you input 4");
+            Console.WriteLine("If you input 4 you could mark or unmark any of your notes into v (completed) ");
+            Console.WriteLine("If you input 5 you could see tutorial again");
         }
     }
 }
--- ConsoleApp4/ToDoList.cs
+++ ConsoleApp4/ToDoList.cs
@@ -1,49 +1,30 @@
-﻿namespace ConsoleApp4
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp4
 {
     internal class ToDoList
     {
         public List<ToDoItem> Tasks = new List<ToDoItem>();
-        public void AddNotes()
+        public void AddNotes(string task)
         {
-            Console.WriteLine("Please add your note");
-            string task = Console.ReadLine();
-            if (task == string.Empty)
-            {
-                PrintText();
-            }
             Tasks.Add(new ToDoItem(task));
-            Console.WriteLine("Your note has been added");
-            PrintText();
         }
 
-        public void RemoveNotes()
+        public void RemoveNotes(string removeItem)
         {
-            Console.WriteLine("Please write number of note which you want to erase");
-            string removeItem = Console.ReadLine();
             if (int.TryParse(removeItem, out int userInput))
             {
-                if (Tasks.Count == 0)
-                {
-                    Console.WriteLine("There is no notes to remove");
-                }
-                else
+                if (Tasks.Count > 0)
                 {
                     Tasks.RemoveAt(userInput - 1);
-                    Console.WriteLine("Your note was removed");
                 }
             }
-            PrintText();
         }
 
-        public void ShowNotes()
+        public void ShowNotes(string mark)
         {
             int currentNumber = 1;
-            string mark = "x";
-            if (Tasks.Count == 0)
-            {
-                Console.WriteLine("You haven't add any notes yet");
-            }
-            else
+            if (Tasks.Count > 0) 
             {
                 for (int i = 0; i < Tasks.Count; i++)
                 {
@@ -54,47 +35,22 @@
                     Console.WriteLine($"{i + 1}) {Tasks[i].Title} {mark}");
                 }
             }
-            PrintText();
         }
 
-        public void MarkNotes()
+        public void MarkNotes(string userInput, bool check)
         {
             Console.WriteLine("Please write note which you want to mark");
-            string userInput = Console.ReadLine();
             int.TryParse(userInput, out int markItem);
-            Tasks[markItem - 1].IsCompleted = true;
-            Console.WriteLine("Your note has been marked");
-            PrintText();
-        }
-
-        public void UnMarkNotes()
-        {
-            Console.WriteLine("Please write note which you want to unmark");
-            string userInput = Console.ReadLine();
-            int.TryParse(userInput, out int markItem);
-            Tasks[markItem - 1].IsCompleted = false;
-            Console.WriteLine("Your note has been unmarked");
-            PrintText();
-        }
-
-        public void PrintText()
-        {
-            Console.WriteLine("Input 1 to add new task");
-            Console.WriteLine("Input 2 to remove task");
-            Console.WriteLine("Input 3 to see your tasks");
-            Console.WriteLine("Input 4 to mark tasks");
-            Console.WriteLine("Inout 5 to unmark tasks");
-            Console.WriteLine("Input 6 for tutorial");
-        }
-
-        public void Tutorial()
-        {
-            Console.WriteLine("If you input 1 you have to write task which will appear when you using (see tasks) ");
-            Console.WriteLine("If you input 2 you could remove your task (you have to input 3 to see your task's number)");
-            Console.WriteLine("If you input 3 you could see all your tasks and status. You can change status if you input 4");
-            Console.WriteLine("If you input 4 you could mark any of your notes into v (completed) ");
-            Console.WriteLine("if you input 5 you could unmark any of your notes into x (is not completed)");
-            Console.WriteLine("If you input 6 you could see tutorial again");
+            if (Tasks[markItem - 1].IsCompleted == false)
+            {
+                Tasks[markItem - 1].IsCompleted = true;
+                check = true;
+            }
+            else
+            {
+                Tasks[markItem - 1].IsCompleted = false;
+                check = true;
+            }
         }
     }
 }
